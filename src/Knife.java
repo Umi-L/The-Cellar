dc60bc4fbe3fb69a19709524817e942702d9d811
Julian O'Grady <julianogrady07@gmail.com>
2023-11-20 09:02:26 -0500

    added knife class

---
+++ src/Knife.java
@@ -0,0 +1,16 @@
+/*
+ Knife (Abstract)
+#Name: string
+#Price: int
+#Modifier: int
+#UpgradePrice: int
+get&set for all fields
+
+ */
+
+public abstract class Knife {
+    protected String name;
+    protected int price;
+    protected int modifier;
+    protected int upgradePrice;
+}
