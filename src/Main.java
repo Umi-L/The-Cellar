eb209470305644fa4c9bed1fe57b60619ef10343
ellabelm <976942@pdsb.net>
2023-11-20 09:05:30 -0500

    added comment

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import javax.swing.*;
 public class Main {
 	// entry point of program
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world");//ellas comment
 	}
 }
