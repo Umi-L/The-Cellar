c64259b4004436bf9a4f06f0352d453e5febde4e
Julian O'Grady <julianogrady07@gmail.com>
2023-11-21 17:49:53 -0500

    save load system for code review

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
+import java.util.ArrayList;
+
 //Aamir
 public class Main {
+
+	public static Game game;
+
 	// entry point of program
 	public static void main(String[] args) {
 		JFrame GamePage = new GamePage();
