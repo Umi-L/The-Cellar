24d35523104f200726664a6109fa0487bee7aaa5
Julian O'Grady <julianogrady07@gmail.com>
2023-11-21 09:59:40 -0500

    added AIBusiness

---
+++ src/AIBusiness.java
@@ -0,0 +1,23 @@
+/*
+ AIBusiness extends Business
+#level: int
++MakeDecision(): void
+get&set for all fields
+
+ */
+
+public class AIBusiness extends Business{
+    protected int level;
+
+    public void MakeDecision() {
+        // TODO: implement
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
