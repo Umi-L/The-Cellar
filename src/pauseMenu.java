7451b3bd5e35224ce17ed5db6b87381ab897329b
AAMIR KAZI <aamir.ak0197@gmail.com>
2023-11-21 19:53:12 -0500

    created mockup ui for pause menu also incomplete need to make it look better added functionality for load instructions button but doesnt display instructions yet

--- src/pauseMenu.java
+++ src/pauseMenu.java
@@ -1,34 +1,31 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-import java.awt.Font;
-import java.awt.event.ActionListener;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import java.awt.Color;
+import javax.swing.JLabel;
 
-public class startMenu {
+public class pauseMenu {
 	private JFrame frame;
-	
-	
-	public startMenu() {
+
+	public pauseMenu() {
 		frame = new JFrame("");
-		frame.setTitle("The Cellar");
 		frame.setBounds(100, 100, 600, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Start New Game");
+		JButton btnNewButton = new JButton("Resume Game");
 		btnNewButton.setForeground(new Color(0, 0, 0));
 		btnNewButton.setBackground(new Color(255, 18, 29));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 			}
 		});
-		btnNewButton.setBounds(46, 44, 219, 70);
+		btnNewButton.setBounds(46, 44, 219, 42);
 		frame.getContentPane().add(btnNewButton);
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -45,24 +42,34 @@ public class startMenu {
 				scrollPane.setVisible(true);
 				scrollPane.add(textArea);
 				textArea.setText("");
-		
+				
 			}
 		});
-		btnNewButton_1.setBounds(46, 184, 219, 70);
+		btnNewButton_1.setBounds(46, 127, 219, 42);
 		frame.getContentPane().add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("Load Save Game");
+		JButton btnNewButton_2 = new JButton("Restart Game");
 		btnNewButton_2.setForeground(new Color(0, 0, 0));
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_2.setBounds(46, 112, 219, 70);
+		btnNewButton_2.setBounds(46, 86, 219, 42);
 		frame.getContentPane().add(btnNewButton_2);
 		
-		JLabel lblNewLabel = new JLabel("The Cellar");
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
-		lblNewLabel.setBounds(86, 0, 206, 56);
+		JButton btnNewButton_3 = new JButton("Save And Exit");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnNewButton_3.setForeground(Color.BLACK);
+		btnNewButton_3.setBackground(new Color(255, 18, 29));
+		btnNewButton_3.setBounds(46, 170, 219, 42);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JLabel lblNewLabel = new JLabel("GAME PAUSED");
+		lblNewLabel.setBounds(115, 6, 219, 32);
 		frame.getContentPane().add(lblNewLabel);
 		
 		frame.setVisible(true);
@@ -70,12 +77,8 @@ public class startMenu {
 	}
 
 	public static void main(String[] args) {
-		new startMenu();
+		new pauseMenu();
 		// TODO Auto-generated method stub
 
-
-			
-		}
 	}
-
-
+}
