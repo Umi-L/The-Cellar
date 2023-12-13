941735347b525421f46cb283261633ad58f35d73
AAMIR KAZI <aamir.ak0197@gmail.com>
2023-12-13 18:48:01 -0500

    esc key opens pause menu and esc key goes back to game page in pause menu

--- src/TheCellar/GUI/PauseMenuPage.java
+++ src/TheCellar/GUI/PauseMenuPage.java
@@ -6,10 +6,16 @@ import TheCellar.SaveLoadSystem;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.ActionMap;
+import javax.swing.AbstractAction;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 import javax.swing.JLabel;
 import java.awt.Font;
 
@@ -24,6 +30,23 @@ public class PauseMenuPage {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.getContentPane().setLayout(null);
+		
+		frame.setFocusable(true);
+
+        // Add KeyBindings to listen for Escape key press
+        InputMap inputMap = frame.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = frame.getRootPane().getActionMap();
+
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "resumeGame");
+        actionMap.put("resumeGame", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                GamePage window2 = new GamePage();
+                window2.showWindow();
+            }
+        });
+
 		
 		JButton btnNewButton = new JButton("Resume TheCellar.Game");
 		btnNewButton.setForeground(new Color(0, 0, 0));
@@ -168,6 +191,12 @@ public class PauseMenuPage {
 		frame.getContentPane().add(lblNewLabel);
 	
 		frame.setVisible(true);
+		
+	}
+
+
+	public void showWindow() {
+		// TODO Auto-generated method stub
 		
 	}
 
